dffe2e7ac29055166aa6edeb8c56459bd61ba15f
Wexop <mattheurtin@gmail.com>
2022-10-22 16:18:46 +0200

    update

--- src/main/java/net/mcreator/izipistols/procedures/MonsterpistolProjectileHitsBlockProcedure.java
+++ src/main/java/net/mcreator/izipistols/procedures/MonsterpistolProjectileHitsBlockProcedure.java
@@ -1,12 +1,16 @@
 package net.mcreator.izipistols.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.entity.monster.Spider;
+import net.minecraft.world.entity.monster.Skeleton;
+import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.entity.monster.Blaze;
 import net.minecraft.world.entity.animal.horse.ZombieHorse;
 import net.minecraft.world.entity.animal.horse.SkeletonHorse;
 import net.minecraft.world.entity.animal.horse.Llama;
 import net.minecraft.world.entity.animal.horse.Horse;
 import net.minecraft.world.entity.animal.axolotl.Axolotl;
-import net.minecraft.world.entity.animal.Wolf;
 import net.minecraft.world.entity.animal.Turtle;
 import net.minecraft.world.entity.animal.Squid;
 import net.minecraft.world.entity.animal.Sheep;
@@ -14,7 +18,6 @@ import net.minecraft.world.entity.animal.Pig;
 import net.minecraft.world.entity.animal.Fox;
 import net.minecraft.world.entity.animal.Cow;
 import net.minecraft.world.entity.animal.Chicken;
-import net.minecraft.world.entity.animal.Cat;
 import net.minecraft.world.entity.animal.Bee;
 import net.minecraft.world.entity.ambient.Bat;
 import net.minecraft.world.entity.MobSpawnType;
@@ -23,7 +26,10 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.server.level.ServerLevel;
 
-public class AnimalpistolProjectileHitsBlockProcedure {
+import net.mcreator.izipistols.init.IziPistolsModEntities;
+import net.mcreator.izipistols.entity.MobPowderEntity;
+
+public class MonsterpistolProjectileHitsBlockProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
 		double random1 = 0;
 		double roll = 0;
@@ -32,7 +38,7 @@ public class AnimalpistolProjectileHitsBlockProcedure {
 		if (random1 >= 0.5 && random1 < 1) {
 			if (roll >= 0.75 && roll < 1) {
 				if (world instanceof ServerLevel _level) {
-					Entity entityToSpawn = new Cow(EntityType.COW, _level);
+					Entity entityToSpawn = new Zombie(EntityType.ZOMBIE, _level);
 					entityToSpawn.moveTo(x, y, z, world.getRandom().nextFloat() * 360F, 0);
 					if (entityToSpawn instanceof Mob _mobToSpawn)
 						_mobToSpawn.finalizeSpawn(_level, world.getCurrentDifficultyAt(entityToSpawn.blockPosition()), MobSpawnType.MOB_SUMMONED,
@@ -41,7 +47,7 @@ public class AnimalpistolProjectileHitsBlockProcedure {
 				}
 			} else if (roll >= 0.5 && roll < 0.75) {
 				if (world instanceof ServerLevel _level) {
-					Entity entityToSpawn = new Chicken(EntityType.CHICKEN, _level);
+					Entity entityToSpawn = new Skeleton(EntityType.SKELETON, _level);
 					entityToSpawn.moveTo(x, y, z, world.getRandom().nextFloat() * 360F, 0);
 					if (entityToSpawn instanceof Mob _mobToSpawn)
 						_mobToSpawn.finalizeSpawn(_level, world.getCurrentDifficultyAt(entityToSpawn.blockPosition()), MobSpawnType.MOB_SUMMONED,
@@ -50,7 +56,7 @@ public class AnimalpistolProjectileHitsBlockProcedure {
 				}
 			} else if (roll >= 0.25 && roll < 0.5) {
 				if (world instanceof ServerLevel _level) {
-					Entity entityToSpawn = new Pig(EntityType.PIG, _level);
+					Entity entityToSpawn = new Creeper(EntityType.CREEPER, _level);
 					entityToSpawn.moveTo(x, y, z, world.getRandom().nextFloat() * 360F, 0);
 					if (entityToSpawn instanceof Mob _mobToSpawn)
 						_mobToSpawn.finalizeSpawn(_level, world.getCurrentDifficultyAt(entityToSpawn.blockPosition()), MobSpawnType.MOB_SUMMONED,
@@ -59,7 +65,7 @@ public class AnimalpistolProjectileHitsBlockProcedure {
 				}
 			} else {
 				if (world instanceof ServerLevel _level) {
-					Entity entityToSpawn = new Sheep(EntityType.SHEEP, _level);
+					Entity entityToSpawn = new Spider(EntityType.SPIDER, _level);
 					entityToSpawn.moveTo(x, y, z, world.getRandom().nextFloat() * 360F, 0);
 					if (entityToSpawn instanceof Mob _mobToSpawn)
 						_mobToSpawn.finalizeSpawn(_level, world.getCurrentDifficultyAt(entityToSpawn.blockPosition()), MobSpawnType.MOB_SUMMONED,
@@ -70,7 +76,7 @@ public class AnimalpistolProjectileHitsBlockProcedure {
 		} else if (random1 >= 0.3 && random1 < 0.5) {
 			if (roll >= 0.75 && roll < 1) {
 				if (world instanceof ServerLevel _level) {
-					Entity entityToSpawn = new Cat(EntityType.CAT, _level);
+					Entity entityToSpawn = new Blaze(EntityType.BLAZE, _level);
 					entityToSpawn.moveTo(x, y, z, world.getRandom().nextFloat() * 360F, 0);
 					if (entityToSpawn instanceof Mob _mobToSpawn)
 						_mobToSpawn.finalizeSpawn(_level, world.getCurrentDifficultyAt(entityToSpawn.blockPosition()), MobSpawnType.MOB_SUMMONED,
@@ -79,7 +85,7 @@ public class AnimalpistolProjectileHitsBlockProcedure {
 				}
 			} else if (roll >= 0.5 && roll < 0.75) {
 				if (world instanceof ServerLevel _level) {
-					Entity entityToSpawn = new Wolf(EntityType.WOLF, _level);
+					Entity entityToSpawn = new MobPowderEntity(IziPistolsModEntities.MOB_POWDER.get(), _level);
 					entityToSpawn.moveTo(x, y, z, world.getRandom().nextFloat() * 360F, 0);
 					if (entityToSpawn instanceof Mob _mobToSpawn)
 						_mobToSpawn.finalizeSpawn(_level, world.getCurrentDifficultyAt(entityToSpawn.blockPosition()), MobSpawnType.MOB_SUMMONED,
@@ -107,7 +113,7 @@ public class AnimalpistolProjectileHitsBlockProcedure {
 			}
 		} else if (random1 >= 0.2 && random1 < 0.3) {
 			if (world instanceof ServerLevel _level) {
-				Entity entityToSpawn = new Llama(EntityType.LLAMA, _level);
+				Entity entityToSpawn = new MobPowderEntity(IziPistolsModEntities.MOB_POWDER.get(), _level);
 				entityToSpawn.moveTo(x, y, z, world.getRandom().nextFloat() * 360F, 0);
 				if (entityToSpawn instanceof Mob _mobToSpawn)
 					_mobToSpawn.finalizeSpawn(_level, world.getCurrentDifficultyAt(entityToSpawn.blockPosition()), MobSpawnType.MOB_SUMMONED, null,
