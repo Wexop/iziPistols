456e69062d55ae4e3ed8b69bdcbc63d7e2b8e5b4
Wexop <mattheurtin@gmail.com>
2022-10-24 15:41:15 +0200

    add tir pistolet sound

--- src/main/java/net/mcreator/izipistols/entity/MonsterpistolEntity.java
+++ src/main/java/net/mcreator/izipistols/entity/MonsterpistolEntity.java
@@ -93,7 +93,7 @@ public class MonsterpistolEntity extends AbstractArrow implements ItemSupplier {
 		entityarrow.setKnockback(knockback);
 		world.addFreshEntity(entityarrow);
 		world.playSound(null, entity.getX(), entity.getY(), entity.getZ(),
-				ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.arrow.shoot")), SoundSource.PLAYERS, 1,
+				ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("izi_pistols:tir_pistolet")), SoundSource.PLAYERS, 1,
 				1f / (random.nextFloat() * 0.5f + 1) + (power / 2));
 		return entityarrow;
 	}
@@ -110,7 +110,7 @@ public class MonsterpistolEntity extends AbstractArrow implements ItemSupplier {
 		entityarrow.setCritArrow(false);
 		entity.level.addFreshEntity(entityarrow);
 		entity.level.playSound(null, entity.getX(), entity.getY(), entity.getZ(),
-				ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.arrow.shoot")), SoundSource.PLAYERS, 1,
+				ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("izi_pistols:tir_pistolet")), SoundSource.PLAYERS, 1,
 				1f / (RandomSource.create().nextFloat() * 0.5f + 1));
 		return entityarrow;
 	}
--- src/main/java/net/mcreator/izipistols/entity/SolidPistolEntity.java
+++ src/main/java/net/mcreator/izipistols/entity/SolidPistolEntity.java
@@ -77,7 +77,7 @@ public class SolidPistolEntity extends AbstractArrow implements ItemSupplier {
 		entityarrow.setKnockback(knockback);
 		world.addFreshEntity(entityarrow);
 		world.playSound(null, entity.getX(), entity.getY(), entity.getZ(),
-				ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.arrow.shoot")), SoundSource.PLAYERS, 1,
+				ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("izi_pistols:tir_pistolet")), SoundSource.PLAYERS, 1,
 				1f / (random.nextFloat() * 0.5f + 1) + (power / 2));
 		return entityarrow;
 	}
@@ -94,7 +94,7 @@ public class SolidPistolEntity extends AbstractArrow implements ItemSupplier {
 		entityarrow.setCritArrow(false);
 		entity.level.addFreshEntity(entityarrow);
 		entity.level.playSound(null, entity.getX(), entity.getY(), entity.getZ(),
-				ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.arrow.shoot")), SoundSource.PLAYERS, 1,
+				ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("izi_pistols:tir_pistolet")), SoundSource.PLAYERS, 1,
 				1f / (RandomSource.create().nextFloat() * 0.5f + 1));
 		return entityarrow;
 	}
